7f9740874de5b1c49851948631484e480ce39601
Somesh Thakur <somesh_thakur@epam.com>
2020-12-06 16:55:28 +0530

    [Task 5.1] Add course service to perform CRUD operations

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CourseService } from '../services/course.service';
 import { Course } from '../shared/course/course';
 
 @Component({
@@ -11,58 +12,10 @@ export class CoursesComponent implements OnInit {
   orderBy = false;
   filterCourseStr: string;
 
-  constructor() {
-    console.log('Constructor');
-  }
+  constructor(private courseService: CourseService) {}
 
   ngOnInit(): void {
-    const freshCourse = new Date();
-    freshCourse.setDate(freshCourse.getDate() - 10);
-    const upComingCourse = new Date();
-    upComingCourse.setDate(upComingCourse.getDate() + 10);
-    this.courses = [
-      {
-        id: '1',
-        title: 'Angular Course',
-        duration: '45',
-        createdDate: freshCourse.toDateString(),
-        topRated: true,
-        description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ultricies facilisis lorem, pulvinar sollicitudin ligula aliquam non. Pellentesque ante ligula, mollis et tellus in, accumsan faucibus mi. Integer et neque non lorem bibendum tempus et sed lectus. Ut dapibus congue odio at placerat. Ut tristique fringilla eros, fermentum faucibus est maximus eget. Fusce et porttitor eros. Mauris ornare sagittis pretium. Vivamus egestas nisi eget nulla ultrices, et rutrum arcu venenatis.',
-      },
-      {
-        id: '2',
-        title: 'React Course',
-        duration: '60',
-        createdDate: new Date(2020, 0, 10).toDateString(),
-        description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ultricies facilisis lorem, pulvinar sollicitudin ligula aliquam non. Pellentesque ante ligula, mollis et tellus in, accumsan faucibus mi. Integer et neque non lorem bibendum tempus et sed lectus. Ut dapibus congue odio at placerat. Ut tristique fringilla eros, fermentum faucibus est maximus eget. Fusce et porttitor eros. Mauris ornare sagittis pretium. Vivamus egestas nisi eget nulla ultrices, et rutrum arcu venenatis.',
-      },
-      {
-        id: '3',
-        title: 'Android Course',
-        duration: '120',
-        createdDate: upComingCourse.toDateString(),
-        description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ultricies facilisis lorem, pulvinar sollicitudin ligula aliquam non. Pellentesque ante ligula, mollis et tellus in, accumsan faucibus mi. Integer et neque non lorem bibendum tempus et sed lectus. Ut dapibus congue odio at placerat. Ut tristique fringilla eros, fermentum faucibus est maximus eget. Fusce et porttitor eros. Mauris ornare sagittis pretium. Vivamus egestas nisi eget nulla ultrices, et rutrum arcu venenatis.',
-      },
-      {
-        id: '4',
-        title: 'IOS Course',
-        duration: '150',
-        createdDate: new Date(2019, 12, 30).toDateString(),
-        description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ultricies facilisis lorem, pulvinar sollicitudin ligula aliquam non. Pellentesque ante ligula, mollis et tellus in, accumsan faucibus mi. Integer et neque non lorem bibendum tempus et sed lectus. Ut dapibus congue odio at placerat. Ut tristique fringilla eros, fermentum faucibus est maximus eget. Fusce et porttitor eros. Mauris ornare sagittis pretium. Vivamus egestas nisi eget nulla ultrices, et rutrum arcu venenatis.',
-      },
-      {
-        id: '5',
-        title: 'Web Basics Course',
-        duration: '20',
-        createdDate: new Date(2020, 4, 25).toDateString(),
-        description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ultricies facilisis lorem, pulvinar sollicitudin ligula aliquam non. Pellentesque ante ligula, mollis et tellus in, accumsan faucibus mi. Integer et neque non lorem bibendum tempus et sed lectus. Ut dapibus congue odio at placerat. Ut tristique fringilla eros, fermentum faucibus est maximus eget. Fusce et porttitor eros. Mauris ornare sagittis pretium. Vivamus egestas nisi eget nulla ultrices, et rutrum arcu venenatis.',
-      },
-    ];
+    this.courses = this.courseService.getList();
   }
 
   loadMore() {
@@ -70,7 +23,12 @@ export class CoursesComponent implements OnInit {
   }
 
   onCourseDelete(id) {
-    console.log('Delete click at course id : ', id);
+    const confirmDelete = window.confirm(
+      'Do you really want to delete the course?'
+    );
+    if (confirmDelete) {
+      this.courseService.removeItem(id);
+    }
   }
   toggleOrderBy() {
     this.orderBy = !this.orderBy;
---
+++ src/app/services/course.service.ts
@@ -0,0 +1,93 @@
+import { Injectable } from '@angular/core';
+import { Course } from '../shared/course/course';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CourseService {
+  courses: Course[];
+  id: number;
+  constructor() {
+    const freshCourse = new Date();
+    freshCourse.setDate(freshCourse.getDate() - 10);
+    const upComingCourse = new Date();
+    upComingCourse.setDate(upComingCourse.getDate() + 10);
+    this.courses = [
+      {
+        id: '1',
+        title: 'Angular Course',
+        duration: '45',
+        createdDate: freshCourse.toDateString(),
+        topRated: true,
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ultricies facilisis lorem, pulvinar sollicitudin ligula aliquam non. Pellentesque ante ligula, mollis et tellus in, accumsan faucibus mi. Integer et neque non lorem bibendum tempus et sed lectus. Ut dapibus congue odio at placerat. Ut tristique fringilla eros, fermentum faucibus est maximus eget. Fusce et porttitor eros. Mauris ornare sagittis pretium. Vivamus egestas nisi eget nulla ultrices, et rutrum arcu venenatis.',
+      },
+      {
+        id: '2',
+        title: 'React Course',
+        duration: '60',
+        createdDate: new Date(2020, 0, 10).toDateString(),
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ultricies facilisis lorem, pulvinar sollicitudin ligula aliquam non. Pellentesque ante ligula, mollis et tellus in, accumsan faucibus mi. Integer et neque non lorem bibendum tempus et sed lectus. Ut dapibus congue odio at placerat. Ut tristique fringilla eros, fermentum faucibus est maximus eget. Fusce et porttitor eros. Mauris ornare sagittis pretium. Vivamus egestas nisi eget nulla ultrices, et rutrum arcu venenatis.',
+      },
+      {
+        id: '3',
+        title: 'Android Course',
+        duration: '120',
+        createdDate: upComingCourse.toDateString(),
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ultricies facilisis lorem, pulvinar sollicitudin ligula aliquam non. Pellentesque ante ligula, mollis et tellus in, accumsan faucibus mi. Integer et neque non lorem bibendum tempus et sed lectus. Ut dapibus congue odio at placerat. Ut tristique fringilla eros, fermentum faucibus est maximus eget. Fusce et porttitor eros. Mauris ornare sagittis pretium. Vivamus egestas nisi eget nulla ultrices, et rutrum arcu venenatis.',
+      },
+      {
+        id: '4',
+        title: 'IOS Course',
+        duration: '150',
+        createdDate: new Date(2019, 12, 30).toDateString(),
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ultricies facilisis lorem, pulvinar sollicitudin ligula aliquam non. Pellentesque ante ligula, mollis et tellus in, accumsan faucibus mi. Integer et neque non lorem bibendum tempus et sed lectus. Ut dapibus congue odio at placerat. Ut tristique fringilla eros, fermentum faucibus est maximus eget. Fusce et porttitor eros. Mauris ornare sagittis pretium. Vivamus egestas nisi eget nulla ultrices, et rutrum arcu venenatis.',
+      },
+      {
+        id: '5',
+        title: 'Web Basics Course',
+        duration: '20',
+        createdDate: new Date(2020, 4, 25).toDateString(),
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ultricies facilisis lorem, pulvinar sollicitudin ligula aliquam non. Pellentesque ante ligula, mollis et tellus in, accumsan faucibus mi. Integer et neque non lorem bibendum tempus et sed lectus. Ut dapibus congue odio at placerat. Ut tristique fringilla eros, fermentum faucibus est maximus eget. Fusce et porttitor eros. Mauris ornare sagittis pretium. Vivamus egestas nisi eget nulla ultrices, et rutrum arcu venenatis.',
+      },
+    ];
+  }
+  getList() {
+    return this.courses;
+  }
+  createCourse(
+    title: string,
+    createdDate: string,
+    duration: string,
+    description: string,
+    topRated: boolean
+  ) {
+    this.courses.push({
+      id: String(this.id),
+      title,
+      createdDate,
+      duration,
+      description,
+      topRated,
+    });
+  }
+  getItemById(id) {
+    return this.courses.find((course) => course.id === id);
+  }
+  updateItem(id, course: Course) {
+    const index = this.courses.findIndex((course) => course.id === id);
+    if (index > -1) {
+      this.courses.splice(index, 1, course);
+    }
+  }
+  removeItem(id) {
+    const index = this.courses.findIndex((course) => course.id === id);
+    if (index > -1) {
+      this.courses.splice(index, 1);
+    }
+  }
+}
